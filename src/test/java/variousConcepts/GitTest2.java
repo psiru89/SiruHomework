bbdaa6b8b33da43afe2fa2963ce32da5eb9382ef
psiru89 <psiru89@gmail.com>
2023-04-27 11:33:19 -0600

    committed 3

---
+++ src/test/java/variousConcepts/GitTest2.java
@@ -0,0 +1,24 @@
+package variousConcepts;
+
+public class GitTest2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("get test");
+		System.out.println("get test1");
+		System.out.println("get test2");
+		System.out.println("get test3");
+		System.out.println("get test4");
+
+		System.out.println("get test5");
+		System.out.println("get test6");
+		System.out.println("get test7");
+		System.out.println("get test8");
+
+		
+		
+		
+
+	}
+
+}
